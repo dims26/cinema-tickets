bb9627c0baf1600e207a02b7e0070b0ea7fb5486
Adedimeji Sebiotimo <dims95@live.com>
2023-06-26 19:54:00 +0100

    refactor(TicketUtilsTest): rewrite for clarity

--- cinema-tickets-java/src/test/java/ticketing/TicketUtilsTest.java
+++ cinema-tickets-java/src/test/java/ticketing/TicketUtilsTest.java
@@ -15,98 +15,98 @@ public class TicketUtilsTest {
 
     @Test
     public void givenNullQuantity_whenCalcPrice_thenThrows() {
-        var typeQuantityMap = new HashMap<Type, Integer>();
-        typeQuantityMap.put(ADULT, 2);
-        typeQuantityMap.put(INFANT, null);
-        typeQuantityMap.put(CHILD, 15);
-        var typePriceMap = new HashMap<Type, Integer>();
-        typePriceMap.put(ADULT, 20);
-        typePriceMap.put(CHILD, 10);
-        typePriceMap.put(INFANT, 0);
+        var typeToQuantityMap = new HashMap<Type, Integer>();
+        typeToQuantityMap.put(ADULT, 2);
+        typeToQuantityMap.put(INFANT, null);
+        typeToQuantityMap.put(CHILD, 15);
+        var typeToPriceMap = new HashMap<Type, Integer>();
+        typeToPriceMap.put(ADULT, 20);
+        typeToPriceMap.put(CHILD, 10);
+        typeToPriceMap.put(INFANT, 0);
 
         var exception = assertThrows(IllegalStateException.class,
-                () -> TicketUtils.calculateTotalPrice(typeQuantityMap, typePriceMap));
+                () -> TicketUtils.calculateTotalPrice(typeToQuantityMap, typeToPriceMap));
         assertTrue(exception.getMessage().toLowerCase(Locale.ROOT)
                 .contains("must not be null"));
     }
 
     @Test
     public void givenQueriedNullPrice_whenCalcPrice_thenThrows() {
-        var typeQuantityMap = new HashMap<Type, Integer>();
-        typeQuantityMap.put(ADULT, 2);
-        typeQuantityMap.put(INFANT, 3);
-        typeQuantityMap.put(CHILD, 15);
-        var typePriceMap = new HashMap<Type, Integer>();
-        typePriceMap.put(ADULT, 20);
-        typePriceMap.put(CHILD, 10);
-        typePriceMap.put(INFANT, null);
+        var typeToQuantityMap = new HashMap<Type, Integer>();
+        typeToQuantityMap.put(ADULT, 2);
+        typeToQuantityMap.put(INFANT, 3);
+        typeToQuantityMap.put(CHILD, 15);
+        var typeToPriceMap = new HashMap<Type, Integer>();
+        typeToPriceMap.put(ADULT, 20);
+        typeToPriceMap.put(CHILD, 10);
+        typeToPriceMap.put(INFANT, null);
 
         var exception = assertThrows(IllegalStateException.class,
-                () -> TicketUtils.calculateTotalPrice(typeQuantityMap, typePriceMap));
+                () -> TicketUtils.calculateTotalPrice(typeToQuantityMap, typeToPriceMap));
         assertTrue(exception.getMessage().toLowerCase(Locale.ROOT)
                 .contains("must not be null"));
     }
 
     @Test
     public void givenMissingPrice_whenCalcPrice_thenThrows() {
-        var typeQuantityMap = new HashMap<Type, Integer>();
-        typeQuantityMap.put(ADULT, 2);
-        typeQuantityMap.put(INFANT, 3);
-        typeQuantityMap.put(CHILD, 15);
-        var typePriceMap = new HashMap<Type, Integer>();
-        typePriceMap.put(ADULT, 20);
-        typePriceMap.put(CHILD, 10);
+        var typeToQuantityMap = new HashMap<Type, Integer>();
+        typeToQuantityMap.put(ADULT, 2);
+        typeToQuantityMap.put(INFANT, 3);
+        typeToQuantityMap.put(CHILD, 15);
+        var typeToPriceMap = new HashMap<Type, Integer>();
+        typeToPriceMap.put(ADULT, 20);
+        typeToPriceMap.put(CHILD, 10);
 
         var exception = assertThrows(IllegalStateException.class,
-                () -> TicketUtils.calculateTotalPrice(typeQuantityMap, typePriceMap));
+                () -> TicketUtils.calculateTotalPrice(typeToQuantityMap, typeToPriceMap));
         assertTrue(exception.getMessage().toLowerCase(Locale.ROOT)
                 .contains("must be a subset"));
     }
 
     @Test
     public void whenCalcPrice_thenComputesCorrectValue() {
-        var typeQuantityMap = new HashMap<Type, Integer>();
-        typeQuantityMap.put(ADULT, 2);
-        typeQuantityMap.put(INFANT, 3);
-        typeQuantityMap.put(CHILD, 15);
-        var typePriceMap = new HashMap<Type, Integer>();
-        typePriceMap.put(ADULT, 20);
-        typePriceMap.put(CHILD, 10);
-        typePriceMap.put(INFANT, 0);
-        var expected = (typeQuantityMap.get(ADULT) * typePriceMap.get(ADULT))
-                + (typeQuantityMap.get(CHILD) * typePriceMap.get(CHILD))
-                + (typeQuantityMap.get(INFANT) * typePriceMap.get(INFANT));
+        var typeToQuantityMap = new HashMap<Type, Integer>();
+        typeToQuantityMap.put(ADULT, 2);
+        typeToQuantityMap.put(INFANT, 3);
+        typeToQuantityMap.put(CHILD, 15);
+        var typeToPriceMap = new HashMap<Type, Integer>();
+        typeToPriceMap.put(ADULT, 20);
+        typeToPriceMap.put(CHILD, 10);
+        typeToPriceMap.put(INFANT, 0);
+        var expected = (typeToQuantityMap.get(ADULT) * typeToPriceMap.get(ADULT))
+                + (typeToQuantityMap.get(CHILD) * typeToPriceMap.get(CHILD))
+                + (typeToQuantityMap.get(INFANT) * typeToPriceMap.get(INFANT));
 
-        var actual = TicketUtils.calculateTotalPrice(typeQuantityMap, typePriceMap);
+        var actual = TicketUtils.calculateTotalPrice(typeToQuantityMap, typeToPriceMap);
 
         assertEquals(expected, actual);
     }
 
     @Test
     public void givenNullQuantity_whenCalcNumSeats_thenThrows() {
-        var typeQuantityMap = new HashMap<Type, Integer>();
-        typeQuantityMap.put(ADULT, 2);
-        typeQuantityMap.put(INFANT, null);
-        typeQuantityMap.put(CHILD, 15);
+        var typeToQuantityMap = new HashMap<Type, Integer>();
+        typeToQuantityMap.put(ADULT, 2);
+        typeToQuantityMap.put(INFANT, null);
+        typeToQuantityMap.put(CHILD, 15);
         var eligibleSeating = Set.of(ADULT, CHILD);
 
         var exception = assertThrows(IllegalStateException.class,
-                () -> TicketUtils.calculateRequiredSeats(typeQuantityMap, eligibleSeating));
+                () -> TicketUtils.calculateRequiredSeats(typeToQuantityMap, eligibleSeating));
         assertTrue(exception.getMessage().toLowerCase(Locale.ROOT)
                 .contains("must not be null"));
     }
 
     @Test
     public void whenCalcNumSeats_thenComputesCorrectValue() {
-        var typeQuantityMap = new HashMap<Type, Integer>();
-        typeQuantityMap.put(ADULT, 2);
-        typeQuantityMap.put(INFANT, 3);
-        typeQuantityMap.put(CHILD, 15);
+        var typeToQuantityMap = new HashMap<Type, Integer>();
+        typeToQuantityMap.put(ADULT, 2);
+        typeToQuantityMap.put(INFANT, 3);
+        typeToQuantityMap.put(CHILD, 15);
         var eligibleSeating = Set.of(ADULT, CHILD);
-        var expected = typeQuantityMap.get(ADULT)
-                + typeQuantityMap.get(CHILD);
+        var expected = typeToQuantityMap.get(ADULT)
+                + typeToQuantityMap.get(CHILD);
 
-        var actual = TicketUtils.calculateRequiredSeats(typeQuantityMap, eligibleSeating);
+        var actual = TicketUtils.calculateRequiredSeats(typeToQuantityMap, eligibleSeating);
 
         assertEquals(expected, actual);
     }
